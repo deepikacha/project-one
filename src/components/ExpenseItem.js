02dd70a5bd525b96ef008e810c50824adb0051fb
deepikacha <deepikachalla9>
2025-05-01 17:19:43 +0530

    complex js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,15 @@
 function ExpenseItem(){
-    return(
-        <p>This is expenseItem!</p>
-    )
+    return (
+        <div>
+          <div>15 August 2023</div>
+          <div>Delhi</div>
+          <div>
+            <h2>Book</h2>
+          <div>$10</div>
+        </div>
+        </div>
+      )
 
 }
 export default ExpenseItem;
-//component1
+//component2
