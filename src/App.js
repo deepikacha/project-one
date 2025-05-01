59f537f1a71d0559c92817e1473cfc4c7a141bfd
deepikacha <deepikachalla9>
2025-05-01 20:21:52 +0530

    using props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import ExpenseItem from './components/ExpenseItem'
+// Write your code at relevant places in the code below:
+
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
-    <div >
-      <h1>Let's get started</h1>
-      <ExpenseItem/>
+    <div>
+      <h1>Let's get Started</h1>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        location="Bangalore"
+        title="Insurance"
+        price="50"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        location="Delhi"
+        title="Book"
+        price="20"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        location="Hyderabad"
+        title="Pen"
+        price="5"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        location="Mumbai"
+        title="Laptop"
+        price="200"
+      ></ExpenseItem>
     </div>
   );
 }
 
 export default App;
-//app1
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
-import './ExpenseItem.css'
-function ExpenseItem(){
-    return (
-        <div className="expense-item">
-          <div>15 August 2023</div>
-          <div>Delhi</div>
-          <div className="expense-item__description">
-            <h2>Book</h2>
-          <div className="expense-item__price">$10</div>
-        </div>
-        </div>
-      )
+// Write your code at relevant places in the code below:
 
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__location">{props.location}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+    </div>
+  );
 }
+
 export default ExpenseItem;
-//component2
