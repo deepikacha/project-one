ce3b7a391ee06d63d268757a3685c8ef5b1148c4
deepikacha <deepikachalla9>
2025-05-02 01:41:45 +0530

    useStste hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,21 @@
 // Write your code here
+import React,{useState} from "react"
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 
 export default function ExpenseItem(props) {
+  const [title,setTitle]=useState(props.title);
   function buttonClickHandler(event){
-    console.log("button clicked");
+    setTitle("New Title");
   }
  
   return (
     <Card className="expense-item">
       <ExpenseDate calenderDate={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       <div className="expense-item__price">${props.price}</div>
       <button onClick={buttonClickHandler}>Change Title</button>
     </div>
