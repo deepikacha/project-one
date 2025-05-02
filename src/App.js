36aa81659207651f4942a2ee9a9868588d78b978
deepikacha <deepikachalla9>
2025-05-02 16:02:39 +0530

    rendering expenses

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-// Write your code at the relevant places in the code below:
+// Write your code at relevant places in the code below:
 
-import ExpenseItem from "./components/Expenses/ExpenseItem";
-import Expenses from "./components/Expenses/Expenses"
-import NewExpense from './components/NewExpense/NewExpense'
+import React,{useState} from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const saveExpenseData=(expenseData)=>{
-    console.log(expenseData)
+const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
+    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
+    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
+  ]);
 
-  }
-
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpense)=>[...prevExpense,expense])
+  };
 
   return (
     <div>
-      <h1>Let's get Started</h1>
-      <NewExpense onSaveExpense={saveExpenseData}/>
-    <Expenses />
-      
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
